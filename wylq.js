161388100f75d00caea5c2b66e3824fcecd1c72e
jack911jie <jack911jie@qq.com>
2020-02-24 17:04:16 +0800

    calculate tiangandizhi

--- wylq.js
+++ wylq.js
@@ -1,174 +1,220 @@
-function  cal(){
-    console.log('begin test');
-    year=$('#y').val();
-    month=$('#m').val();
-    day=$('#d').val();
-    // alert(year+month+day);
+var name_Arr = ["立春", "雨水", "惊蛰", "春分", "清明", "谷雨",
+"立夏", "小满", "芒种", "夏至", "小暑", "大暑",
+"立秋", "处暑", "白露", "秋分", "寒露", "霜降",
+"立冬", "小雪", "大雪", "冬至", "小寒", "大寒"]
+
+var tg={0:'甲',1:'乙',2:'丙',3:'丁',4:'戊',5:'己',6:'庚',7:'辛',8:'壬',9:'癸'}
+var dz={0:'子',1:'丑',2:'寅',3:'卯',4:'辰',5:'巳',6:'午',7:'未',8:'申',9:'酉',10:'戌',11:'亥'}
+
+function showgz(y,m,d,h=-1){    
+    var  year=$('#y').val();
+    var month=$('#m').val();
+    var day=$('#d').val();
+    var hour=$('#h').val();
+    if(hour==""){
+        hour=-1
+    }
+    var res=cal_gz(year,month,day,hour)
+
+    $('#show').text(res)
     
-    var _y=year.split('');
-    var y_4=_y[_y.length-1];
-    console.log(year+'年是：'+togz(year))
-    togz()
 }
 
-function togz(n){
-    console.log(n)
-    var tg={0:'甲',1:'乙',2:'丙',3:'丁',4:'戊',5:'己',6:'庚',7:'辛',8:'壬',9:'癸'}
-    var dz={0:'子',1:'丑',2:'寅',3:'卯',4:'辰',5:'巳',6:'午',7:'未',8:'申',9:'酉',10:'戌',11:'亥'}
-    if(n%10>3){
-        var N=n%10-3;
+
+function  cal_gz(y,m,d,h=-1,zishi=0){
+     console.log('begin test');
+   
+
+    //子时的处理：
+    //0：23-0点也属于后一天（默认）
+    //1:   23-0点属于当天
+    if(zishi==0){
+        if(h==23){
+            console.log('该时辰算入下一天的子时')
+            var tmp_d=new Date(y,m,d)
+            tmp_d.setDate(tmp_d.getDate()+1)
+            var year=tmp_d.getFullYear()
+            var month=tmp_d.getMonth()
+            var day=tmp_d.getDate()
+            var hour=h
+        }else{            
+            var year=y
+            var month=m
+            var day=d
+            var hour=h
+        }
+    }else if(zishi==1){
+        if(h==23){
+            console.log('该时辰算当天的子时')
+        }
+            var year=y
+            var month=m
+            var day=d
+            var hour=h
+    }
+    var result_cal= togz(year,month,day,hour)
+    var htmltxt=[y,m,d,h==-1?"":h,result_cal]
+    console.log(htmltxt)
+    return htmltxt
+}
+
+function togz(y,m,d,h){
+    if(y%10>3){
+        var N=y%10-3;
     }else{
-        var N=n%10-3+10;
+        var N=y%10-3+10;
     }
 
-    if(n%12>3){
-        var M=n%12-3;
+    if(y%12>3){
+        var M=y%12-3;
     }else{
-        var M=n%12-3+12;
+        var M=y%12-3+12;
     }
 
-    N=N-1;
+    N=N-1;  //索引从0开始
     M=M-1;
-    console.log(N,M);
-    g=tg[N];
-    z=dz[M];
-    var gz=[g,z];
-    // 公元后的算法：
-    // 年干=N-3(N>3)或N-3+10(N≤3)，N=年号除以10的余数=年号个位数。
-    // 年支=N-3(N>3)或N-3+12(N≤3)，N=年号除以12的余数。
 
-    // 公元前的算法：
-    // 年干=8-N(N﹤8)或8-N+10(N≧8)，N=年号除以10的余数=年号个位数。
-    // 年支=10-N(N<10)或10-N+12(N≧10)，N=年号除以12的余数。
+    jq_In=jq2(y,m,d)
 
-    // C取决于节气和年份。
+    //按节气来计算月份：    
+    for(i=0;i<name_Arr.length;i++){
+        if(jq_In[0][0]==name_Arr[i]){
+            if(i%2==0){
+                var month=i+1  
+            }else{
+                var month=i
+            }
+            break
+        }        
+    }
 
-    // 日期计算：
+    month=(month+1)/2
 
-    // 通式寿星公式——[Y×D+C]-L
+    M_g=month-1     //列表索引从0开始
+    M_z=month-1+2  //地支从寅开始，初始列表从子开始，所以+2。
 
-    // Y=年代数的后2位、D=0.2422、L=闰年数、C取决于节气和年份。
+    //年的干支序数
+    Y_g=N
+    Y_z=M
 
-    // 21世纪立春的C值=3.87
-    // 例如：2017年的立春日期
+//月的干支序数
+//    月份:
+// 甲己之年丙作首  2
+// 乙庚之年戊为头  4
+// 丙辛之岁寻庚上  6
+// 丁壬——壬 8
+// 戊癸——甲 0
 
-    // [17×0.2422+3.87]-[（17-1）/4]=7.9874-4=3
+//根据以上口诀调整月干排序的参数：
+    if(N==0||N==5){
+        var re_m=2
+    }else if(N==1||N==6){
+        var re_m=4
+    }else if(N==2||N==7){
+        var re_m=6
+    }else if(N==3||N==8){
+        var re_m=8
+    }else if(N==4||N==9){
+        var re_m=0
+    }
 
-    // 所以2017年的立春日期是2月3日。（注：计算结果只保留整数）
+    //日的干支序数
+    var yd_this=y.toString()+'-1-1'
+    var today=y.toString()+'-'+m.toString()+'-'+d.toString()
+    var res=parseInt(comparedate(today,yd_this)/1000/86400)
+    var yd_this_gz=yd(y)
+    var D_g=res%10+yd_this_gz[0]>9?res%10+yd_this_gz[0]-10:res%10+yd_this_gz[0] //根据当年的元旦干支计算，以当年元旦干支的序号重新定位。
+    var D_z=res%12+yd_this_gz[1]>11?res%12+yd_this_gz[1]-12:res%12+yd_this_gz[1]
+
+
+    //时的干支序数：
+    //  时干（看日干而定）：
+    // 甲己——甲  0
+    // 乙庚——丙 2
+    // 丙辛——戊 4
+    // 丁壬——庚 6
+    // 戊癸——壬 8
+   
+    if(D_g==0||D_g==5){
+        var re_h=0
+    }else if(D_g==1||D_g==6){
+        var re_h=2
+    }else if(D_g==2||D_g==7){
+        var re_h=4
+    }else if(D_g==3||D_g==8){
+        var re_h=6
+    }else if(D_g==4||D_g==9){
+        var re_h=8
+    }
+
+    if(h%2!=0){
+        h_0=(h+1)/2
+    }else{
+        h_0=h/2
+    }
+   
+    if(h==-1 || h==" "){  //有“时”的输入
+        H_g=''
+        re_h=''
+        H_z=''
+    }else{
+        H_g=h_0
+        H_z=h_0       
+    }
+
+    var dis_Y_g,dis_Y_z,dis_M_g,dis_M_z,dis_D_g,dis_D_z
+
+  if(jq_In[0][0]=='冬至'||jq_In[0][0]=='小寒'||jq_In[0][0]=='大寒'){  //处理当年1月份、2月份立春前的日期
+    Y_g=Y_g-1
+    Y_z=Y_z-1    
+    M_g=M_g
+    }
+
+    try{
+        dis_Y_g=tg[deal_odr(Y_g,'g')]
+        dis_Y_z=dz[deal_odr(Y_z,'z')]
+        dis_M_g=tg[deal_odr(M_g+re_m,'g')]
+        dis_M_z=dz[deal_odr(M_z,'z')]
+        dis_D_g=tg[deal_odr(D_g,'g')]
+        dis_D_z=dz[deal_odr(D_z,'z')]
+        dis_H_g=tg[deal_odr(H_g+re_h,'g')]
+        dis_H_z=dz[deal_odr(H_z,'z')]
+    }catch{
+        ;
+    }
+
+    var gz=[dis_Y_g,dis_Y_z,dis_M_g,dis_M_z,dis_D_g,dis_D_z,dis_H_g,dis_H_z]
+    for(i=0;i<gz.length;i++){
+        if(typeof(gz[i])=='undefined'||gz[i]==-1){  //时间未输入，赋值为空
+            gz[i]=' '
+        }
+    }
+
+    // console.log(gz)
+
     return gz
 
 }
 
-function jieqi(y,m,d){
-    var C_Arr_21= [3.87, 18.73, 5.63, 20.646, 4.81, 20.1,
-        5.52, 21.04, 5.678, 21.37, 7.108, 22.83,
-        7.5, 23.13, 7.646, 23.042, 8.318, 23.438,
-        7.438, 22.36, 7.18, 21.94, 5.4055, 20.12]
-
-    var  C_Arr_20 = [4.6295,19.4599,6.3826,21.4155,5.59,20.888,
-            6.318,21.86,6.5,22.2,7.928,23.65,
-            28.35,23.95,8.44,23.822,9.098,24.218,
-            8.218,23.08,7.9,22.6,6.11,20.84]
-
-    
-
-    var name_Arr = ["立春", "雨水", "惊蛰", "春分", "清明", "谷雨",
-            "立夏", "小满", "芒种", "夏至", "小暑", "大暑",
-            "立秋", "处暑", "白露", "秋分", "寒露", "霜降",
-            "立冬", "小雪", "大雪", "冬至", "小寒", "大寒"]
-
-
-            if(y<2000){
-                var C_Arr=C_Arr_20
-            }else if(y>2000){
-                var C_Arr=C_Arr_21
-            }else{   //2000的大小寒用1999年的数据，1999年属于20世纪
-                var C_Arr=C_Arr_21
-                C_Arr[22]=C_Arr_20[22]
-                C_Arr[23]=C_Arr_20[23]    
-            }
-
-            // console.log(C_Arr)
-            var L=parseInt(y/4)-parseInt(y/100)+parseInt(y/400)
-            console.log(y+'年，'+(parseInt(y/100)+1)+'世纪')
-
-            var jieqi_date_list=new Array()
-            for(i=0;i<24;i++){
-                var jieqi_m=parseInt(i/2)+2
-                
-
-                if(i<22){
-                    var jieqi_d=parseInt(y*0.2422+C_Arr[i])-L
-                    var jieqi_ymd=y+'-'+jieqi_m+'-'+jieqi_d
-                    console.log(jieqi_ymd+'：'+name_Arr[i])
-                    jieqi_date_list.push(jieqi_ymd)
-                }else{
-                    var jieqi_d=parseInt((y-1)*0.2422+C_Arr[i])-L  //实际上是去年小寒大寒，所以如果要计算的是2000年的小寒大寒，应该用20世纪的C来计算
-                    var jieqi_ymd=y+'-'+(jieqi_m-12)+'-'+jieqi_d
-                    console.log(jieqi_ymd+'：'+name_Arr[i])
-                    jieqi_date_list.push(jieqi_ymd)
-                }
-            }
-
-            var j=new Array()
-            j.push(jieqi_date_list[22])
-            j.push(jieqi_date_list[23])
-            for(i=0;i<22;i++){
-                j.push(jieqi_date_list[i])
-            }
-
-            // console.log(j)
-
-            var j_name= new Array()
-            j_name.push(name_Arr[22])
-            j_name.push(name_Arr[23])
-            for(i=0;i<24;i++){
-                j_name.push(name_Arr[i])
-            }
-
-            var inputdate=y+'-'+m+'-'+d
-            for(i=0;i<24;i++){
-                    var res=comparedate(j[i],inputdate)
-                    // console.log(res)
-                    if(res>=0){
-                        var loc=i-1
-                        break
-                    }
-            }
-
-            
-            console.log(j[loc]+'：'+j_name[loc]+' 和 '+j[loc+1]+'：'+j_name[loc+1])
-
-
-            // for(i=0;i<)
-            // (1)通式公计算式——[Y×D+C]-L
-
-            // Y=年代数、D=0.2422、L=闰年数、C取决于节气和年份。
-            
-            // (2)设要计算的年为A,则到A年为止（含A年）的闰年数为：
-            //             闰年的定义：
-
-            // 能被4整除的非世纪年(1900年虽能被4整除但因为是世纪年 故不是闰年)
-
-            // 能被400整除的世纪年(2000年可以被400整除 故是闰年)
-                        
-            // 闰年数=INT(A/4)-INT(A/100)+INT(A/400),INT为取整数函数
-            
-            // 本世纪立春的C值=4.475，求2017年的立春日期如下：
-            
-            // [2017×0.2422+4.475]-[2017/4-2107/100+2017/400]=492-489=3
-            
-            // 所以2017年的立春日期是2月3日.
-            
-
+function deal_odr(n,j){
+    if(j=='z'){
+        if(n<0){
+            n=n+12
+        }else if(n>11){
+            n=n-12
+        }
+    }else if(j=='g'){
+        if(n<0){
+            n=n+10
+        }else if(n>9){
+            n=n-10
+        }
+    }
+    return n
 }
 
 function jq2(y,m,d){
     var ymd=y+"-"+m+"-"+d
-    var name_Arr = ["立春", "雨水", "惊蛰", "春分", "清明", "谷雨",
-    "立夏", "小满", "芒种", "夏至", "小暑", "大暑",
-    "立秋", "处暑", "白露", "秋分", "寒露", "霜降",
-    "立冬", "小雪", "大雪", "冬至", "小寒", "大寒"]
     var jieqiData=JSON.parse(data)
     var jqdata=jieqiData[y]
 
@@ -184,29 +230,54 @@ function jq2(y,m,d){
     for(i=0;i<22;i++){
         jqinfo.push(info[i])
     }
-
+    var t =new Array()
     for(i=0;i<jqinfo.length;i++){
-        res=comparedate(jqinfo[i][1],ymd)
-        if(res>=0){
-            t=jqinfo[i-1][0]+"--"+jqinfo[i][0]
-            console.log(ymd+"："+t)
-            break
+        // console.log(jqinfo[i],ymd)
+        var result=comparedate(jqinfo[i][1],ymd)
+
+        if(result>0){ 
+            try{
+                t.push(jqinfo[i-1][0])
+                t.push(jqinfo[i][0])            
+                break     
+            }
+            catch(err){    //处理1月份小寒以前的日期
+                // console.log("res:",result,err.message)
+                t.push('冬至')
+                t.push('小寒')            
+                break     
+            }                            
         }
     }
-    
+    if(result<0){
+        t.push('冬至')
+        t.push('小寒')        
+    }
+
+// console.log(ymd+"："+t,res)
+    var out=[t,jqinfo]
+    return out
 
 }
 
-// var result
-// $.ajax({
-//     url: "src/1800-2100jieqi.json",//json文件位置
-//     type: "GET",//请求方式为get
-//     dataType: "json", //返回数据格式为json
-//     success: function(data) {//请求成功完成后要执行的方法 
-//         console.log('success')
-//         result=data
-//     }
-//  })
+function yd(b){
+    var a=1899 //从1900年的元旦推算到今年元旦,1900年为：己亥年 丙子月 甲戌日
+    b=b-1
+    var A_run=parseInt(a/4)-parseInt(a/100)+parseInt(a/400)
+    var A_ping=a-A_run
+    var B_run=parseInt(b/4)-parseInt(b/100)+parseInt(b/400)
+    var B_ping=b-B_run
+    var x=B_ping-A_ping
+    var y=B_run-A_run
+    var t=x*365+y*366
+
+
+    var di=tg[t%10]+dz[t%12+10>11?t%12+10-12:t%12]  //从1900元旦为甲戌，戌为第10位
+    var di_out=[t%10,t%12+10>11?t%12+10-12:t%12]
+    // console.log(di,di_out)
+    return di_out
+
+}
 
 function getoutjq(a,b){
     var jq= new Array()
@@ -219,7 +290,6 @@ function getoutjq(a,b){
     // var re=/农历.+/      //农历
     // var s=re.exec(a[b])[0]
     // jq.push(s)
-
     return jq
 }
 
@@ -230,4 +300,9 @@ function comparedate(o,n){
     return res
 }
 
-jq2(2017,04,25)
+
+// cal_gz(2020,3,2,2,zishi=0)
+// cal_gz(2020,3,2,2,zishi=1)
+// cal_gz(2020,3,2,23,zishi=0)
+// cal_gz(2020,3,2,23,zishi=1)
+// cal_gz(2020,3,2)
